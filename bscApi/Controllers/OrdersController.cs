7c929966d627c3f7364cbf51ef4460ad3ea5f819
Cristina Aguilar Rodriguez <to.aguilar.rodriguez.cristina@gmail.com>
2025-05-23 02:33:58 -0600

    feat: add new order controller and update user controller

---
+++ bscApi/Controllers/OrdersController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using bscApi.Helpers;
+using bscApi.Models;
+using bscApi.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+
+namespace bscApi.Controllers
+{
+    [Route("orders")]
+    [Authorize]
+    public class OrdersController : ControllerBase
+    {
+        private readonly bscContext _context;
+        public OrdersController(bscContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ObjectResult> GetOrder()
+        {
+            try
+            {
+                var order = await _context.VwGetOrders.ToListAsync();
+                return new OkObjectResult(order);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+        [HttpPost]
+        public async Task<ObjectResult> AddOrder([FromBody] OrderDTO data)
+        {
+            try
+            {
+                var pendingStatus = await _context.CatEstatusPedidos.Where(x => x.EstatusPedidos == "Pendiente").Select(x => x.IdEstatusPedidos).FirstOrDefaultAsync();
+                var order = new Pedido
+                {
+                    IdUsuario = 1,
+                    IdCliente = data.IdCliente,
+                    IdEstatusPedido = pendingStatus,
+                    Cantidad = data.Cantidad,
+                    PrecioTotal = data.Precio
+                };
+                await _context.Pedidos.AddAsync(order);
+                await _context.SaveChangesAsync();
+                return new OkObjectResult(order);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+        
+        
+    }
+}
--- bscApi/Controllers/UsersController.cs
+++ bscApi/Controllers/UsersController.cs
@@ -34,13 +34,38 @@ namespace bscApi.Controllers
                 return BadRequest(e);
             }
         }
-        [HttpPut]
-        public async Task<IActionResult> UpdateUsers()
+        [HttpGet("clients")]
+        public async Task<ObjectResult> GetClients()
         {
             try
             {
-                var users = await _context.Usuarios.ToListAsync();
-                return Ok(users);
+                var clients = await _context.VwGetClients.ToListAsync();
+                return Ok(clients);
+                
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+        [HttpPut("{idUsuario}")]
+        public async Task<ObjectResult> ChangeStatus([FromRoute] int idUsuario)
+        {
+            try
+            {
+                var user = await _context.Usuarios.Where(x => x.IdPersona == idUsuario).FirstOrDefaultAsync();
+                var activeS = await _context.CatEstatusUsuarios.Where(x => x.EstatusUsuarios == "Activo").Select(x => x.IdEstatusUsuario).FirstOrDefaultAsync();
+                var inactiveS = await _context.CatEstatusUsuarios.Where(x => x.EstatusUsuarios == "Inactivo").Select(x => x.IdEstatusUsuario).FirstOrDefaultAsync();
+
+                if (activeS == user.IdEstatusUsuario)
+                {
+                    user.IdEstatusUsuario = inactiveS;
+                } else {
+                    user.IdEstatusUsuario = activeS;
+                }
+                _context.Usuarios.Update(user);
+                await _context.SaveChangesAsync();
+                return new OkObjectResult(user);
                 
             }
             catch (Exception e)
