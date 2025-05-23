d89b2f262c18c7282bab6becf73c8ed41705a48e
Cristina Aguilar Rodriguez <to.aguilar.rodriguez.cristina@gmail.com>
2025-05-23 02:33:18 -0600

    feat: add new order dto

---
+++ bscApi/DTO/OrderDTO.cs
@@ -0,0 +1,9 @@
+namespace bscApi.DTO
+{
+    public class OrderDTO
+    {
+        public int IdCliente { get; set; }
+        public int Cantidad { get; set; }
+        public int Precio { get; set; }
+    }
+}
